95b857a2561f6a76e0d8796cb751771347e16863
João V. Dias <diasvictorj@gmail.com>
2022-05-17 14:16:04 -0300

    Correcoes requisito 12,13 e 19

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,9 +1,11 @@
-db.produtos.updateMany(
-    { criadoPor: null },
-    { $set: { criadoPor: "Ronald Mcdonald" } },
-  );
+db.produtos.updateMany({
+  criadoPor: null,
+}, {
+  $set: { criadoPor: "Ronald McDonald" },
+});
+
 db.produtos.find({}, {
-     nome: 1,
-     criadoPor: 1,
-     _id: 0,
+  nome: 1,
+  criadoPor: 1,
+  _id: 0,
 });
